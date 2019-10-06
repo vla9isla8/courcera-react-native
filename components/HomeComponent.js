e87753a7978a0250db449c840811cf3481107dd3
VKalashnikov <vkalashnikov@croc.ru>
2019-10-06 23:55:14 +0800

    Animations

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {ScrollView, View, Text } from "react-native";
+import {View, Text, Animated, Easing } from "react-native";
 import { Card } from "react-native-elements";
 import {connect} from "react-redux";
 import { baseUrl } from "../datasource";
@@ -38,12 +38,37 @@ function RenderItem({item,onPress,loading,error,style}) {
 
 class Home extends Component {
 
+
+    constructor(props) {
+        super(props);
+        this.animatedValue = new Animated.Value(0);
+    }
+
     static navigationOptions = {
         title: "Home"
     }
 
+
+    componentDidMount() {
+        this.animate();
+    }
+
+    animate() {
+        this.animatedValue.setValue(0);
+        Animated.timing(this.animatedValue, {
+            toValue: 8,
+            duration: 8000,
+            easing: Easing.linear
+        }).start(() => this.animate());
+    }
+
+
     render() {
 
+        const xpos1 = this.animatedValue.interpolate({
+            inputRange: [0, 8],
+            outputRange: [1200, -1200]
+        });
 
         const featuredDish = this.props.dishes.data.find(({featured})=> featured);
         const featuredLeader = this.props.leaders.data.find(({featured})=> featured);
@@ -52,26 +77,32 @@ class Home extends Component {
         const {navigate} = this.props.navigation;
 
         return(
-            <ScrollView style={{flex: 1}}>
-                <RenderItem 
-                    style={{flex: 1}}
-                    item={featuredDish} 
-                    loading={this.props.dishes.loading}
-                    error={this.props.dishes.error}
-                    onPress={(id) => navigate('Dishdetail', {dishId: id})} />
-                <RenderItem 
-                    style={{flex: 1}}
-                    item={featuredPromo} 
-                    loading={this.props.promotions.loading}
-                    error={this.props.promotions.error}
-                />
-                <RenderItem 
-                    style={{flex: 1}}
-                    item={featuredLeader} 
-                    loading={this.props.leaders.loading}
-                    error={this.props.leaders.error}
-                />
-            </ScrollView>
+            <View style={{flex: 1, flexDirection: "row", justifyContent: "center"}}>
+                <Animated.View style={{width: "100%", transform: [{ translateX: xpos1}]}}>
+                    <RenderItem 
+                        style={{flex: 1}}
+                        item={featuredDish} 
+                        loading={this.props.dishes.loading}
+                        error={this.props.dishes.error}
+                        onPress={(id) => navigate('Dishdetail', {dishId: id})} />
+                </Animated.View>
+                <Animated.View style={{width: "100%", transform: [{ translateX: xpos1}]}}>
+                    <RenderItem 
+                        style={{flex: 1}}
+                        item={featuredPromo} 
+                        loading={this.props.promotions.loading}
+                        error={this.props.promotions.error}
+                    />
+                </Animated.View>
+                <Animated.View style={{width: "100%", transform: [{ translateX: xpos1}]}}>
+                    <RenderItem 
+                        style={{flex: 1}}
+                        item={featuredLeader} 
+                        loading={this.props.leaders.loading}
+                        error={this.props.leaders.error}
+                    />
+                </Animated.View>
+            </View>
         );
     }
 }
